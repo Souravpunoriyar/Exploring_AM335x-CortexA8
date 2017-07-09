d7caab100a8f12580fdee879135f8ec66170cb7d
Sourav Punoriyar <souravpunoriyar@gmail.com>
2017-07-09 16:08:27 -0400

    core_init_debug+enable l1 inst cache

--- core_init.c
+++ core_init.c
@@ -3,65 +3,69 @@
 #include "bbb.h"
 #include "arm.h"
 #include "serial.h"
+#include "core_init.h"
 
-void read_SCTLR(void)
+/*to enable debugs #define CORE_INIT_DEBUG*/
+
+#ifdef CORE_INIT_DEBUG
+  #define SERIAL_DEBUG(a) serial_tx(a)
+#else
+  #define SERIAL_DEBUG(a)
+#endif
+
+
+
+/*Why only to enable L1 instruction cache, Disable L1 Data Cache
+ * answer: https://stackoverflow.com/questions/21262014/arm-bootloader-disable-mmu-and-caches
+ * */
+
+unsigned long read_SCTLR(void) /*Reading CP15 system control register*/
 {
   unsigned long system_register = 0;
   char hex_string[50] ={0};
   __asm__ __volatile__("mrc p15, 0, %0, c1, c0, 0 \n" : "=r" (system_register) : : );
-  serial_tx("SCTLR_Register = ");
+  SERIAL_DEBUG("SCTLR_Register = ");
   get_hex_string( system_register , hex_string, 50);
-  serial_tx(hex_string);
-  serial_tx("\r\n");
+  SERIAL_DEBUG(hex_string);
+  SERIAL_DEBUG("\r\n");
+  return system_register;
 }
 
-
-void toggle_cache_bit(void)
+void enable_Icache_L1(void)
 {
-	
-  read_SCTLR();
-    
-    asm(
+ unsigned long system_register = 0; 
+ 
+ /*check status of system register*/
+ system_register = read_SCTLR();
+
+ /*if I-cache disable then enable I-cache L1*/
+  if( !(BIT_STATUS( system_register, 12))) 
+  {
+     SERIAL_DEBUG("Enable Icache \r\n ");
+      asm(
 	 "MRC p15, 0, r1, c1, c0, 0\n\t" // Read System Control Register (SCTLR)
      "BIC r1, r1, #1\n\t" // mmu off
      "BIC r1, r1, #(1 << 12)\n\t" //i-cache off
      "BIC r1, r1, #(1 << 2)\n\t" //d-cache & L2-$ off
      "MCR p15, 0, r1, c1, c0, 0\n\t" //Write System Control Register (SCTLR)
      );
-   
-   read_SCTLR();
-   asm(
+     
+     asm(
       "MRC p15, 0, r0, c1, c0, 0\n\t"      // System control register
       "ORR r0, r0, #1 << 12\n\t"          // Instruction cache enable
       "ORR r0, r0, #1 << 11\n\t"         //Program flow prediction
       "MCR p15, 0, r0, c1, c0, 0\n\t"   //System control register
      );
-   read_SCTLR();
+     	  
+  }
+  else
+    SERIAL_DEBUG("Icache already enabled \r\n ");   	   
+   
 }
-
-#if 0
-void enable_Icache_L1(void)
-{
- unsigned long system_register = 0; 
- unsigned long system_result = 0
- /*check status of system register*/
- //read_SCTLR();
-
- /*if I-cache disable then enable I-cache L1*/
- /*in C
-  * if(result = ((r0 >> position)&(1)))
-  *    bit is set
-  * */
- //__asm__ __volatile__("mrc p15, 0, %0, c1, c0, 0 \n" : "=r" (system_register) : : );
- //__asm__ __volatile__(");
-  
-}
-#endif
-
 
 void core_init(void)
 {
 	interrupt_init(); //setup interrupt vector table
-	toggle_cache_bit();
+	enable_Icache_L1(); //disable mmu and l1-data cache , only enable l1 instruction cache
 		
 }
--- core_init.h
+++ core_init.h
@@ -1,1 +1,3 @@
 void core_init(void);
+unsigned long read_SCTLR(void);
+#define BIT_STATUS(value,position) (((value >> position) & 1 )?1:0)
